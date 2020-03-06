513384621c6e6134fefe33f483168ac506cf242a
Adam Kaplan <adam.kaplan@redhat.com>
2020-03-06 11:22:25 -0500

    use same source structure

---
+++ relpath/badger.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+echo "Source tree:"
+echo "---------------------------"
+tree
+echo "---------------------------"
+echo "BADGER BADGER BADGER BADGER"
+echo "BADGER BADGER BADGER BADGER"
+echo "BADGER BADGER BADGER BADGER"
+cat badger/badger.txt
+echo "BADGER BADGER BADGER BADGER"
+echo "BADGER BADGER BADGER BADGER"
+echo "BADGER BADGER BADGER BADGER"
+cat badger/mushroom/snake.txt
+cat badger/mushroom.txt
--- relpath/mushroom.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-echo "mushroom/mushroom"
-cat mushroom/mushroom.txt
